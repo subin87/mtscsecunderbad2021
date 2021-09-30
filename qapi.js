9e81deb5ac522b622081796b1ebce9a1d8811ebb
subin87 <60582569+subin87@users.noreply.github.com>
2021-09-30 14:33:10 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fbfd3c5cae167001e06eb1b",
+				"5fbfd3c5cae167001e06eb1d"
+			]
+		},
+		"5fbfd3c5cae167001e06eb1b": {
+			"id": "5fbfd3c5cae167001e06eb1b",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fbfd3c5cae167001e06eb1d": {
+			"id": "5fbfd3c5cae167001e06eb1d",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
